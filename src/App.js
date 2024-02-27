b1f58d6c8a92f6a1f214252de8a17dffe6d83d00
hshy1839 <hongjeongmin1839@gmail.com>
2024-02-28 04:15:23 +0800

    deploy

--- src/App.js
+++ src/App.js
@@ -10,11 +10,7 @@ import Passwordppl from './component/Passwordppl';
 import Page2 from './component/Page2';
 import Page3 from './component/Page3';
 import Page4 from './component/Page4';
-<<<<<<< HEAD
 import Page5 from './component/Page5';
-=======
-import Contents from './component/Contents';
->>>>>>> a3bd1bdcdeca36b63643270293839d933d6338a2
 import Cms from './admin/Cms';
 import Cmss from './admin/Cmss';
 import MyPage from './component/MyPage';
@@ -24,7 +20,6 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Routes>
-<<<<<<< HEAD
           <Route path="/main" element={<Main />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
@@ -34,16 +29,6 @@ function App() {
           <Route path="/page3" element={<Page3 />} />
           <Route path="/page4" element={<Page4 />} />
           <Route path="/Page5" element={<Page5 />} />
-=======
-          <Route path="/" element={ <> <Header /> <Main /> </> }/>
-          <Route path="/main" element={<> <Header /> <Main /> </>} />
-          <Route path="/login" element={<> <Header /><Login /> </>} />
-          <Route path="/signup" element={<> <Header /><Signup /> </>} />
-          <Route path="/page1" element={<> <Header /><Page1 /> </>} />
-          <Route path="/page2" element={<> <Header /><Page2 /> </>} />
-          <Route path="/page3" element={<> <Header /><Page3 /> </>} />
-          <Route path="/page4" element={<> <Header /><Page4 /> </>} />
->>>>>>> a3bd1bdcdeca36b63643270293839d933d6338a2
           <Route path="/cmss" element={<Cmss />} />
           <Route path="/cms" element={<Cms />} />
           <Route path="/contents" element={<> <Header /><Contents /> </>} />
